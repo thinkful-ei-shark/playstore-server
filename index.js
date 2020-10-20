ac1f9b8782165307a9c1fb59e4c335ac7ed0dd8e
Taylor Phelps <tphelps1993@gmail.com>
2020-10-20 12:56:26 -0400

    init commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+app.use(morgan('common'));
+app.use(cors());
+
+
+const apps = require('./playstore.js');
+
+
+app.get('/apps', (req, res) => {
+  const {search = '', sort, genres } = req.query;
+
+  if (sort, genres) {
+    if (!['title', 'genre'].includes(sort)) {
+      return res.status(400).send('Sort must be one of title or genre');
+    }
+  }
+
+  let results = apps.filter(app => 
+    app.App.toLowerCase().includes(search.toLowerCase())
+  );
+
+  if (sort, genres) {
+    results.sort((a, b) => {
+      return a[sort, genres] > b[sort, genres] ? 1 : a[sort, genres] < b[sort, genres] ? -1 : 0;
+    });
+  }
+  res.json(results);
+});
+
+
+app.listen(8000, () => {
+  console.log('Server is listening on PORT 8000');
+});
